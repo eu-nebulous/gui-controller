70bb912218691360bcb5014eb6754afc6b36cb88
Vasilis Kefalas <vkefalas@existanze.com>
2024-08-27 18:08:05 +0300

    Platforms are automatically added when app is migrated

--- modules/platforms/index.js
+++ modules/platforms/index.js
@@ -38,9 +38,29 @@ module.exports = {
         group: {
             basics: {
                 label: 'Basics',
-                fields: ['uuid','public','sortOrder']
+                fields: ['uuid','provider_name','public','sortOrder']
             }
         }
+    },
+    init(self) {
+        self.apos.migration.add('insert-default-platforms', async () => {
+            try {
+                self.apos.util.log('Starting default platforms insertion migration...');
+                const req = self.apos.task.getReq();
+
+                const existingPlatforms = await self.find(req, {}).toCount();
+
+                if (existingPlatforms === 0) {
+                    self.apos.util.log('No platforms found. Inserting default platforms...');
+                    await self.insertDefaultPlatforms();
+                } else {
+                    self.apos.util.log('Platforms already exist. Skipping default insertion.');
+                }
+                self.apos.util.log('Finished default platforms insertion migration.');
+            } catch (error) {
+                self.apos.util.error('Error during migration:', error);
+            }
+        });
     },
     handlers(self) {
         return {
@@ -69,6 +89,55 @@ module.exports = {
                     } catch (error) {
                         throw self.apos.error('notfound', 'Platforms not found');
                     }
+                }
+            }
+        }
+    },
+    methods(self) {
+        return {
+            async insertDefaultPlatforms() {
+                const req = self.apos.task.getReq();
+                const existingPlatforms = await self.find(req, {}).toCount();
+
+                if (existingPlatforms === 0) {
+                    const defaultPlatforms = [
+                        {
+                            title: 'AWS',
+                            uuid: '8fb2bb14-0adf-4d91-a7bd-cb6980dbc738',
+                            provider_name: 'aws-ec2',
+                            public: true,
+                            sortOrder: 1
+                        },
+                        {
+                            title: 'GCP',
+                            uuid: '0283769e-f3a5-40a0-8cb4-59aafc8a903c',
+                            provider_name: 'Google Cloud Platform',
+                            public: true,
+                            sortOrder: 2
+                        },
+                        {
+                            title: 'OPENSTACK',
+                            uuid: '9f59fa75-7945-4c41-b7f8-8e04b846365e',
+                            provider_name: 'Openstack',
+                            public: true,
+                            sortOrder: 3
+                        },
+                        {
+                            title: 'BYON',
+                            uuid: '1954c7c2-4cf5-41f0-8e3c-fa9b6b54a0fa',
+                            provider_name: 'Byon',
+                            public: true,
+                            sortOrder: 4
+                        }
+                    ];
+
+                    for (const platform of defaultPlatforms) {
+                        await self.insert(req, platform);
+                    }
+
+                    self.apos.util.log('Default platforms inserted.');
+                } else {
+                    self.apos.util.log('Platforms already exist, skipping default insertion.');
                 }
             }
         }
